cc713ca4ed0f0b10186470f9e9d0f2fb820725e7
Mohammad Mirzaei <mirzaei_1378@yahoo.com>
2024-08-26 11:13:15 -0700

    timestamp for messages fixed

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { format } from "date-fns"
 import { useRef, useState } from "react"
 
 interface Props {
@@ -9,6 +10,11 @@ interface Props {
 export default function Messages({initialMessages, sessionId}:Props){
     const [messages, setMessages] = useState<Message[]>(initialMessages)
     const scrollDownRef = useRef<HTMLDivElement | null>(null)
+
+    function formatTimestap(timestamp:number){
+        return format(timestamp, "HH:mm")
+    }
+
     return(
         <div id="messages" className='flex h-full flex-1 flex-col-reverse gap-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch'>
             <div ref={scrollDownRef} >
@@ -26,7 +32,7 @@ export default function Messages({initialMessages, sessionId}:Props){
                                 <div className={"flex flex-col space-y-2 text-base max-w-xs mx-2 " + (isCurrentUser ? "order-1 items-end" : "order-2 items-start")}>
                                     <span className={"px-4 py-2 rounded-lg inline-block " + (isCurrentUser ? " bg-indigo-600 text-white " : " bg-gray-200 text-gray-900 ") + (!hasNextMessageFromSameUser && isCurrentUser? " rounded-br-none " : " ") + (!hasNextMessageFromSameUser && !isCurrentUser ? " rounded-bl-none " : " ")}>
                                         {message.text + " "}
-                                        <span className="ml-2 text-xs text-gray-400">{message.timestamp}</span>
+                                        <span className="ml-2 text-xs text-gray-400">{formatTimestap(message.timestamp)}</span>
                                     </span>
                                 </div>
                             </div>
